d48d6a094078815eb501561e7271a9a03a11f85b
GiorgioAscione <acergio95@libero.it>
2022-04-04 16:41:38 +0200

    Add project files.

---
+++ bookstore-csharp/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// ESERCIZIO BOOKSTORE C# 
+
+Console.WriteLine(" Informazioni sul libro: Clean Code ");
+
+// Dichiarazioni variabili sulle informazioni del libro
+
+string titoloLibro = "Clean Code";
+Console.WriteLine(" Titolo libro: " + titoloLibro);
+
+string autore = "Robert C. Martin";
+Console.WriteLine(" Autore del libro: " + autore);
+
+long ISBN = 9780132350884;
+Console.WriteLine(" ISBN: " + ISBN);
+
+int numeroDiPagine = 431;
+Console.WriteLine(" Numero di pagine: " + numeroDiPagine);
+
+int pesoGrammiLibro = 668;
+Console.WriteLine(" Peso libro: " + pesoGrammiLibro);
+
+double lunghezzaLibro = 17.91;
+Console.WriteLine(" Lunghezza libro: " + lunghezzaLibro);
+
+double larghezzaLibro = 2.54;
+Console.WriteLine(" Larghezza libro: " + larghezzaLibro);
+
+double profonditàLibro = 23.5;
+Console.WriteLine(" Profondità libro: " + profonditàLibro);
+
+// Informazioni Amazon
+
+double valutazioneMedia = 4.7;
+Console.WriteLine(" Valutazione in media: " + valutazioneMedia);
+
+int numeroRecensioni = 3388;
+Console.WriteLine(" Numero di recensioni: " + numeroRecensioni);
+
+string versioneKindle = "Si";
+Console.WriteLine(" Versione kindle: " + versioneKindle);
+
+string versioneFlessibile = "Si";
+Console.WriteLine(" Versione flessibile: " + versioneFlessibile);
